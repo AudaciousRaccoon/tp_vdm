75d6053ec6d0125428394f92c8a21270f16fd3cd
ZauggL <zaugg.loic@gmail.com>
2023-11-22 16:32:20 +0100

    Ajout de ping.sh fonctionnel sent.conf est vide sent.sh surveille sent.service est un exemple

---
+++ ping.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+while [ a = a ];do
+	
+	# Exraction de la liste des IP dans la BDD
+	IPs=$(cut -d: -f2 bdd)
+
+
+	# Les séparateurs de la liste sont des passages à la ligne
+	IFS=$'\n'
+	for IP in $IPs;do
+
+		# Extraction de l'espace disque de la machine
+		espace=$(cat bdd | grep $IP | grep -o -E "[^:]*$")
+		# Extraction de l'état de la machine
+		etat=$(grep $IP bdd | cut -d: -f3)
+
+		ping -c 1 $IP -w 1 > /dev/null
+		retour=$?
+		# On modifie bdd uniquement s'il y a lieu
+		if [ $retour -eq 0 ] && [ $etat != "UP" ];then
+			sed -E "s/$IP:(.*):(.*)$/$IP:UP:$espace/g" -i bdd
+
+		elif [ $retour -eq 1 ] && [ $etat != "DOWN" ];then
+			sed -E "s/$IP:(.*):(.*)$/$IP:DOWN:$espace/g" -i bdd
+		fi
+
+	done
+	sleep 60
+done
