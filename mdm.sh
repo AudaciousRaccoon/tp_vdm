e98336b04d58829e1cdc890ded7cd92d8e4117f7
AudaciousRaccoon <antton.domercq@laposte.net>
2023-11-23 09:58:24 +0100

    on a avancé sur mdm.sh, reste un probleme avec le modify

--- mdm.sh
+++ mdm.sh
@@ -2,39 +2,63 @@
 
 echo "Bienvenue dans MDM, le manager de machines que le monde entier nous envie"
 
-#sftp -P 2222 admin_vdm@10.125.23.75  << EOF 
+#sftp -P 2222 admin_vdm@10.125.23.75  << chocolat 
 #		get bdd 
 #                exit 
-#EOF 
+#chocolat
 
-continue=True
+loop=True
 
-while [ $continue = True ]
+while [ $loop = True ]
 do
-	read -p "Entrez votre commande : " action arg1 arg2 arg3
+	read -p "Entrez votre commande : " action arg1 arg2 arg3 arg4 
 
 	if [ $action = stop ]
 	then
-		continue=false
+		loop=false
 	elif [ $action = add ]
 	then
 		echo "$arg1:$arg2:down:$arg3" >> bdd
 	
 	elif [ $action = list ]
 	then
+		if [ -z $arg1 ]
+		then
+			cat bdd
+		else
 		cat bdd | grep $arg1 | cut -d: -f1,2
+		fi
 	elif [ $action = remove ]
 	then 
-		cat bdd | sed -E "s/^$arg1.*?$//g" > bdd
+		cat bdd | sed -E "s/^$arg1[^$]*$//g" > tmp
+		grep : tmp > bdd
+		rm tmp
 	
+	elif [ $action = change ]
+	then
+		ip=$(cat bdd | grep ^$arg1 | cut -d: -f2 )
+		
+		ip_status=$(cat bdd | grep ^$arg1 | cut -d: -f3 )
+
+		
+	       	if [ $arg3 = --name ]
+		then
+			cat bdd | sed -E "s/^$arg1[^$]*$/$arg4:$ip:$ip_status:$arg2/g" > tmp
+			cat tmp > bdd
+			rm tmp
+		elif [ $arg3 = --group ]
+		then
+			cat bdd | sed -E "s/^$arg1[^$]*$/$arg1:$ip:$ip_status:$arg4/g" > tmp
+			cat tmp > bdd
+			rm tmp
+		fi
 	fi	
 done
 
-
-#sftp -P 2222 admin_vdm@10.125.23.75  << EOF 
-#		put  bdd 
+#sftp -P 2222 admin_vdm@10.125.23.75  << vanille
+#		put  bdd2 
 #                exit 
-#EOF 
+#vanille
 
 
 
